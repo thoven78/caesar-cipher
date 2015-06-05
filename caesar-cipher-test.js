c8ac6619ceb40152a6eedf3b0bd3eb6240e37608
Stevenson Michel <thoven87@gmail.com>
2015-06-05 13:17:12 -0400

    Added Encrypt and decrypt method with test

---
+++ caesar-cipher-test.js
@@ -0,0 +1,16 @@
+import CaesarCipher from './caesar-cipher';
+
+
+let assert = function(condition, test, message) {
+  if (condition !== test) {
+    throw('Expected ' + test + ' , but got ' + condition);
+  }
+  return true;
+};
+
+
+let casear = new CaesarCipher();
+
+assert(casear.encrypt('The quick brown cow'), 'Dro aesmu lbygx myg', 'Expect it to passed');
+
+assert(casear.decrypt('Dro aesmu lbygx myg'), 'The quick brown cow', 'Expect it to passed')
---
+++ caesar-cipher.js
@@ -0,0 +1,27 @@
+export default class CaesarCipher {
+
+  encrypt(text, shift = 10) {
+    let result = '';
+
+    for (let i = 0; i < s.length; i++) {
+
+      let char = s.charCodeAt(i);
+
+      if (char === 32) { // spaces
+        result += ' ';
+      }
+
+  		if (char >= 65 && char <=  90) {
+        result += String.fromCharCode((char - 65 + shift) % 26 + 65);  // Uppercase
+      } else if (char >= 97 && char <= 122) {
+        result += String.fromCharCode((char - 97 + shift) % 26 + 97);  // Lowercase
+      }
+    }
+
+    return result;
+  }
+
+  decrypt(text, shift = 10) {
+    return encrypt((26 - shift) % 26);
+  }
+}
