b672619b105db6c2b1f15f7d23967e63b53b49b1
Stevenson Michel <thoven87@gmail.com>
2015-09-15 13:20:13 -0400

    Renamed test file

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
-import CaesarCipher from './caesar-cipher';
+'use strict';
 
+//import CaesarCipher from './caesar-cipher';
+let CaesarCipher = require('./caesar-cipher');
 
 let assert = function(condition, test, message) {
   if (condition !== test) {
@@ -13,4 +15,4 @@ let casear = new CaesarCipher();
 
 assert(casear.encrypt('The quick brown cow'), 'Dro aesmu lbygx myg', 'Expect it to passed');
 
-assert(casear.decrypt('Dro aesmu lbygx myg'), 'The quick brown cow', 'Expect it to passed')
+assert(casear.decrypt('Dro aesmu lbygx myg'), 'The quick brown cow', 'Expect it to passed');
